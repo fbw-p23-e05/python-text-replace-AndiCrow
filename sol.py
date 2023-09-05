b58d631fdc9a088f1559152c3dec51e8c7cd5002
Andi <andreas.rajchert@dci-student.org>
2023-09-05 13:39:23 +0200

    import re

--- sol.py
+++ sol.py
@@ -1,3 +1,18 @@
 text = "A dogmatic dog buys dogecoin to become rich and buy hotdogs every day."
+# dog = text.replace(" dog ", " cat ")
+# print(f"{dog}")
 
-print(text.replace(" dog ", " cat "))
+
+import re
+
+dog = re.search("dog ", text)
+
+if dog:
+    match_text = dog.group()
+    text = text.replace(match_text, "cat ")
+    print(text)
+else:
+    print("No cats were found")
+
+
+
